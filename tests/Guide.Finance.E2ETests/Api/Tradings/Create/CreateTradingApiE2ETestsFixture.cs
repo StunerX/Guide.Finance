9ecd3fdc43e0908b5e80df457539f8d9381cb722
Cleyton Santos <cleyton.santos@fidelizaplataforma.com>
2023-03-09 20:31:57 -0300

    refactor: fix name

---
+++ tests/Guide.Finance.E2ETests/Api/Tradings/Create/CreateTradingApiE2ETestsFixture.cs
@@ -0,0 +1,14 @@
+﻿using Guide.Finance.E2ETests.Base;
+using Xunit;
+
+namespace Guide.Finance.E2ETests.Api.Tradings.Create;
+
+public class CreateTradingApiE2ETestsFixture : BaseFixture
+{
+    
+}
+
+[CollectionDefinition(nameof(CreateTradingApiE2ETestsFixture))]
+public class CreateCategoryE2ETestsFixtureCollection : ICollectionFixture<CreateTradingApiE2ETestsFixture>
+{
+}
