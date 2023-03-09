3edc7cbe1e84c7869e7bfbdb36fc79a83d10da9f
Cleyton Santos <cleyton.santos@fidelizaplataforma.com>
2023-03-09 11:45:51 -0300

    feat: should create trading

---
+++ src/Guide.Finance.Domain/Entities/Trading.cs
@@ -0,0 +1,17 @@
+﻿namespace Guide.Finance.Domain.Entities;
+
+public class Trading
+{
+    public string Id { get; }
+    public string Symbol { get; }
+    public decimal Price { get; }
+    public DateTime CreatedAt { get; }
+
+    public Trading(string symbol, decimal price)
+    {
+        Id = Guid.NewGuid().ToString();
+        Symbol = symbol;
+        Price = price;
+        CreatedAt = DateTime.Now;
+    }
+}
