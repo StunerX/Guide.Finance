57d03de80cce0c238f12bbe4a3a2a7138010fcc4
Cleyton Santos <cleyton.santos@fidelizaplataforma.com>
2023-03-09 14:10:00 -0300

    test: add integration test for create trading use case

---
+++ src/Guide.Finance.Application/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Guide.Finance.Application.Interfaces;
+using Guide.Finance.EntityFramework;
+
+namespace Guide.Finance.Application.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly GuideFinanceDbContext _context;
+    
+    public UnitOfWork(GuideFinanceDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task Commit(CancellationToken cancellationToken = default)
+    {
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ tests/Guide.Finance.IntegrationTests/Application/UseCases/Tradings/Create/CreateTradingUseCaseTestsFixture.cs
@@ -0,0 +1,14 @@
+﻿using Guide.Finance.IntegrationTests.Base;
+
+namespace Guide.Finance.IntegrationTests.Application.UseCases.Tradings.Create;
+
+public class CreateTradingUseCaseTestsFixture : BaseFixture
+{
+    
+}
+
+[CollectionDefinition(nameof(CreateTradingUseCaseTestsFixture))]
+public class CreateTradingUseCaseTestsFixtureCollection : ICollectionFixture<CreateTradingUseCaseTestsFixture>
+{
+    
+}
