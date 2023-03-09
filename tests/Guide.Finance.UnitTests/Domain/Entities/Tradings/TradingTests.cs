164de7f05bfbd77ce676b06eac5d9291b1b79037
Cleyton Santos <cleyton.santos@fidelizaplataforma.com>
2023-03-09 11:45:59 -0300

    test: should create trading

---
+++ tests/Guide.Finance.UnitTests/Domain/Entities/Tradings/TradingTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Guide.Finance.Domain.Entities;
+
+namespace Guide.Finance.UnitTests.Domain.Entities.Tradings;
+
+public class TradingTests
+{
+    [Fact]
+    public void ShouldCreateTrading()
+    {
+        var trading = new Trading("Trading 1", 10m);
+        trading.Should().NotBeNull();
+        trading.Id.Should().NotBeEmpty();
+        trading.Symbol.Should().Be("Trading 1");
+        trading.Price.Should().Be(10m);
+        trading.CreatedAt.Should().NotBe(default);
+    }
+}
