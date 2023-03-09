6d51ba77601816009b73c384d88db62eae7bb450
Cleyton Santos <cleyton.santos@fidelizaplataforma.com>
2023-03-09 18:04:49 -0300

    test: GetTradingPriceVariationUseCase should return list of calculated price variation

---
+++ tests/Guide.Finance.UnitTests/Application/UseCases/Tradings/Get/GetTradingPriceVariationUseCaseTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using Guide.Finance.Application.UseCases.Tradings.Get;
+using Guide.Finance.Domain.Entities;
+using Guide.Finance.Domain.Interfaces;
+using Moq;
+
+namespace Guide.Finance.UnitTests.Application.UseCases.Tradings.Get;
+
+[Collection(nameof(GetTradingPriceVariationUseCaseTestsFixture))]
+public class GetTradingPriceVariationUseCaseTests
+{
+    private GetTradingPriceVariationUseCaseTestsFixture _fixture;
+
+    public GetTradingPriceVariationUseCaseTests(GetTradingPriceVariationUseCaseTestsFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task GetTradingPriceVariationShouldReturnPriceVariation()
+    {
+        var tradings = new List<Trading>
+        {
+            _fixture.RandomTrading(),
+            _fixture.RandomTrading(),
+            _fixture.RandomTrading(),
+            _fixture.RandomTrading(),
+            _fixture.RandomTrading()
+        };
+
+        var tradingRepository = new Mock<ITradingRepository>();
+        tradingRepository
+            .Setup(x => x.GetAll(CancellationToken.None))
+            .ReturnsAsync(tradings);
+        
+        var getTradingPriceVariationUseCase = new GetTradingPriceVariationUseCase(tradingRepository.Object);
+        var result = await getTradingPriceVariationUseCase
+            .Handle(It.IsAny<GetTradingPriceVariationInput>(), CancellationToken.None);
+        
+        result.Should().NotBeNull();
+        
+        var firstTrading = result.MinBy(x => x.CreatedAt);
+
+        for (var i = 1; i < result.Count(); i++)
+        {
+            var trading = result.ElementAt(i);
+            var variationFromFirst = _fixture.CalculateVariation(trading.Price, firstTrading!.Price);
+            var variationFromLast = _fixture.CalculateVariation(trading.Price, result.ElementAt(i - 1).Price);
+            
+            trading.PercentVariationFromFirst.Should().Be(variationFromFirst);
+            trading.PercentVariationFromLast.Should().Be(variationFromLast);
+        }
+    }
+}
---
+++ tests/Guide.Finance.UnitTests/Application/UseCases/Tradings/Get/GetTradingPriceVariationUseCaseTestsFixture.cs
@@ -0,0 +1,33 @@
+﻿using Bogus;
+using Guide.Finance.Domain.Entities;
+
+namespace Guide.Finance.UnitTests.Application.UseCases.Tradings.Get;
+
+public class GetTradingPriceVariationUseCaseTestsFixture
+{
+    private Faker Faker { get; }
+
+    public GetTradingPriceVariationUseCaseTestsFixture()
+    {
+        Faker = new Faker("pt_BR");
+    }
+    
+    public Trading RandomTrading()
+    {
+        var symbol = Faker.Name.JobArea();
+        var price = Faker.Random.Decimal(0, 1000);
+        var date = Faker.Date.Past();
+        return new Trading(symbol, price, date);
+    }
+    
+    public decimal CalculateVariation(decimal firstPrice, decimal lastPrice)
+    {
+        var result = ((firstPrice / lastPrice) - 1) * 100;
+        return Math.Truncate(result * 100) / 100;
+    }
+}
+
+[CollectionDefinition(nameof(GetTradingPriceVariationUseCaseTestsFixture))]
+public class GetTradingPriceVariationUseCaseTestsFixtureCollection : ICollectionFixture<GetTradingPriceVariationUseCaseTestsFixture>
+{
+}
