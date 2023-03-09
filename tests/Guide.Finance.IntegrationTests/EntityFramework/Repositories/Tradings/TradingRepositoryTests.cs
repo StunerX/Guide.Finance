4515a41475c1baeb5d6ea7cd6db3f29fdc00d89a
Cleyton Santos <cleyton.santos@fidelizaplataforma.com>
2023-03-09 13:43:01 -0300

    test: TradingRepository should create trading

---
+++ tests/Guide.Finance.IntegrationTests/EntityFramework/Repositories/Tradings/TradingRepositoryTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Guide.Finance.Domain.Entities;
+using Guide.Finance.EntityFramework.Repositories;
+
+namespace Guide.Finance.IntegrationTests.EntityFramework.Repositories.Tradings;
+
+[Collection(nameof(TradingRepositoryTestsFixture))]
+public class TradingRepositoryTests
+{
+    private readonly TradingRepositoryTestsFixture _fixture;
+    
+    public TradingRepositoryTests(TradingRepositoryTestsFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    
+    [Fact]
+    public async Task ShouldCreateTrading()
+    {
+        var trading = new Trading("Trading 1", 10m);
+        var dbContext = _fixture.CreateDbContext();
+        var tradingRepository = new TradingRepository(dbContext);
+        await tradingRepository.Create(trading, CancellationToken.None);
+        var result = await dbContext.Tradings.FindAsync(trading.Id);
+        result.Should().NotBeNull();
+        result!.Id.Should().NotBeEmpty();
+        result.Symbol.Should().Be("Trading 1");
+        result.Price.Should().Be(10m);
+        result.CreatedAt.Should().NotBe(default);
+    }
+}
---
+++ tests/Guide.Finance.IntegrationTests/EntityFramework/Repositories/Tradings/TradingRepositoryTestsFixture.cs
@@ -0,0 +1,24 @@
+﻿using Guide.Finance.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace Guide.Finance.IntegrationTests.EntityFramework.Repositories.Tradings;
+
+public class TradingRepositoryTestsFixture
+{
+    public GuideFinanceDbContext CreateDbContext()
+    {
+        var dbContext = new GuideFinanceDbContext(
+            new DbContextOptionsBuilder<GuideFinanceDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options);
+
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+
+        return dbContext;
+    }
+}
+
+[CollectionDefinition(nameof(TradingRepositoryTestsFixture))]
+public class TradingRepositoryTestsFixtureCollection : ICollectionFixture<TradingRepositoryTestsFixture>
+{
+}
