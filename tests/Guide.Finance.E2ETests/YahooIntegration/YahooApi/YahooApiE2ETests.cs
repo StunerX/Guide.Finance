add75d3bf53741a84f08797349f9e770403b6ab6
Cleyton Santos <cleyton.santos@fidelizaplataforma.com>
2023-03-09 22:52:17 -0300

    test: YahooApi Integration

---
+++ tests/Guide.Finance.E2ETests/YahooIntegration/YahooApi/YahooApiE2ETests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Guide.Finance.Yahoo.Integration;
+using Xunit;
+
+namespace Guide.Finance.E2ETests.YahooIntegration.YahooApi;
+
+[Collection(nameof(YahooApiE2ETestsFixture))]
+public class YahooApiE2ETests
+{
+    private readonly YahooApiE2ETestsFixture _fixture;
+    
+    public YahooApiE2ETests(YahooApiE2ETestsFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    
+    [Fact]
+    public async Task ShouldGetTradingsInfo()
+    {
+        var yahooApi = new Yahoo.Integration.YahooApi(_fixture.HttpClient);
+
+        var result = await yahooApi.GetTradingInfo("PETR4.SA", DateTime.Now);
+        result.Should().NotBeNull();
+        result.Should().BeOfType<YahooChartResponse>();
+    }
+}
---
+++ tests/Guide.Finance.E2ETests/YahooIntegration/YahooApi/YahooApiE2ETestsFixture.cs
@@ -0,0 +1,13 @@
+﻿using Guide.Finance.E2ETests.Base;
+using Xunit;
+
+namespace Guide.Finance.E2ETests.YahooIntegration.YahooApi;
+
+public class YahooApiE2ETestsFixture : BaseFixture
+{
+}
+
+[CollectionDefinition(nameof(YahooApiE2ETestsFixture))]
+public class YahooApiE2ETestsFixtureCollection : ICollectionFixture<YahooApiE2ETestsFixture>
+{
+}
